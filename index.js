14880734d5614a988ca0ef3b70c959312a387bf7
imam0321 <imam.hossain0321@gmail.com>
2024-04-16 02:19:03 +0600

    added users model and create users and save the users data on database

--- index.js
+++ index.js
@@ -2,32 +2,46 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
+const User = require("./models/user.model");
+const bcrypt = require('bcryptjs');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.0hgquea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.MG_USER}:${process.env.MG_PASS}@swift-sell-rent.eecu0il.mongodb.net/?retryWrites=true&w=majority&appName=swift-sell-rent`;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // create Users
+    app.post("/signUp", async (req, res) => {
+      const { username, email, password } = req.body;
+      const hashedPassword =  bcrypt.hashSync(password, 10)
+      const newUser = new User({ username, email, password: hashedPassword });
+      try{
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully!" });
+      }
+      catch(error){
+        res.status(500).json(error.message);
+      }
+    });
+
+
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // await client.close();
+    // Ensures that the client will close when you finish/error
+    // await mongoose.disconnect();
   }
 }
 run().catch(console.dir);
